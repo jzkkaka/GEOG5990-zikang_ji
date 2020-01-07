e17980ee995a4294e7897bd9a7f9375364279288
jzkkaka <56165367+jzkkaka@users.noreply.github.com>
2020-01-07 14:33:07 +0000

    work_1
    final code

---
+++ agentframework.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-#
+
+# -------------------------------------------------------------------------------
+# File  : agentframework.py
+# Author:zikang ji
+# Date  : 2019-11-21 08:27
+# Desc  : 
+# -------------------------------------------------------------------------------
+
+import random
+
+
+class Agent:
+    def __init__(self, environment, agents,y,x):
+        self.environment = environment
+        self.store = 0
+        self.agents = agents
+        if x != None:
+            self.x = x
+        else:
+            self.x = random.randint(0,300)
+        if y != None:
+            self.y = y
+        else:
+            self.y = random.randint(0,300)
+
+    def set_x(self, x):
+        self.x = x
+
+    def set_y(self, y):
+        self.y = y
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def set_store(self,store):
+        self.store = store
+
+    def get_store(self):
+        return self.store
+
+    def move(self):
+        if random.random() < 0.5:
+            self.x = (self.x + 1) % 300
+        else:
+            self.x = (self.x - 1) % 300
+        if random.random() < 0.5:
+            self.y = (self.y + 1) % 300
+        else:
+            self.y = (self.y - 1) % 300
+
+    def eat(self):  # can you make it eat what is left?
+        if self.environment[self.y][self.x] > 10:
+            self.environment[self.y][self.x] -= 10
+            self.store += 10
+
+    def distance_between(self, agent):
+        return (((self.x - agent.x) ** 2) +
+                ((self.y - agent.y) ** 2)) ** 0.5
+
+    def share_with_neighbours(self, neighbourhood):
+        for agent in self.agents:
+            dist = self.distance_between(agent)
+            if dist <= neighbourhood:
+                sum = self.store + agent.store
+                ave = sum / 2
+                self.store = ave
+                agent.store = ave
---
+++ final.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-#
+
+import matplotlib
+import matplotlib.pyplot
+import matplotlib.animation
+import tkinter
+import requests
+import bs4
+import agentframework
+import csv
+
+matplotlib.use('TkAgg')
+
+r = requests.get('http://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html')
+content = r.text
+soup = bs4.BeautifulSoup(content, 'html.parser')
+td_ys = soup.find_all(attrs={"class": "y"})
+td_xs = soup.find_all(attrs={"class": "x"})
+#print(td_ys)
+#print(td_xs)
+
+num_of_agents = 10
+num_of_iterations = 100
+agents = []
+
+fig = matplotlib.pyplot.figure(figsize=(10, 10))
+ax = fig.add_axes([0, 0, 1, 1])
+
+# ax.set_autoscale_on(False)
+
+# get enviroment
+environment = []
+with open('in.txt',newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    for row in spamreader:
+        rowlist = []
+        for value in row:
+            rowlist.append(int(value))
+        environment.append(rowlist)
+
+
+for i in range(num_of_agents):
+    y = int(td_ys[i].text)
+    x = int(td_xs[i].text)
+
+    agents.append(agentframework.Agent(environment, agents, y, x))
+
+
+def update(frame_number):
+    fig.clear()
+    
+    for i in range(num_of_agents):
+        agents[i].move()
+        agents[i].eat()
+        agents[i].share_with_neighbours(20)
+
+    for i in range(num_of_agents):
+        matplotlib.pyplot.scatter(agents[i].x, agents[i].y)
+        # print(agents[i].x, agents[i].y)
+    matplotlib.pyplot.imshow(environment)
+
+carry_on = True
+
+def gen_function(b=[0]):
+    a = 0
+    global carry_on
+    while (a < 10) & (carry_on):
+        yield a
+        a = a + 1
+
+def run():
+    animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=True)
+    # matplotlib.pyplot.show()
+    #canvas.draw()
+    canvas.draw()
+
+root = tkinter.Tk()
+root.wm_title("Model")
+menu_bar = tkinter.Menu(root)
+root.config(menu=menu_bar)
+model_menu = tkinter.Menu(menu_bar)
+menu_bar.add_cascade(label="Model", menu=model_menu)
+model_menu.add_command(label="Run model", command=run)
+canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
+canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+
+tkinter.mainloop()
